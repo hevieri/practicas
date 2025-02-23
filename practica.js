b0eba7998d997e439427c1d0d32ac1bd83b24bd4
hevieri <hevieri@duck.com>
2025-02-23 02:37:43 -0300

    ejercicio suma

---
+++ practica.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+function calcularDoble(numero){
+    return numero*2;
+}
+console.log(calcularDoble(5));
+
+
+function sumar(a, b){
+  return a + b;
+}
+console.log(sumar(5,4));
+
+
+function sumar(a,b){
+  return a+b;
+}
+console.log(sumar(5,3));
+
+function sumar(a, b);{
+  return a + b;
+}
+console.log(sumar(5,9));
+
+function sumar(a,b){
+  return(a+b);
+}
+   
+console.log(sumar(3+5));
+
+
+function sumar(a,b){
+  return a+b;
+}
+console.log(suma(3,4));
+
+function sumar(a,b){
+  return a+b;
+}
+console.log (sumar(3,5));
