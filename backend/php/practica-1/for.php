a47d667bb2b76d5f7d6eadab29d707bf6f0e3b6b
hevieri <hevieri@duck.com>
2025-06-29 18:05:18 -0300

    practica

--- backend/php/practica-1/for.php
+++ backend/php/practica-1/for.php
@@ -1,5 +1,24 @@
 <?php
 
+/*
+
+OPC1
+for(var; condicion; increment) {
+    // code to be executed
+}
+
+OPC2
+for(var, condicion, incere|decre):
+    // code to be executed
+endfor;
+    */
+
+
+
+for($i=1; $i<=20; $i++){
+    echo $i."<br>";
+}
+
 ?>
 
     
---
+++ backend/php/practica-1/foreach.php
@@ -0,0 +1,24 @@
+<?php
+/*
+
+OPC1
+foreach($array as $valor) {
+   valor que tendrá en cada iteracion del array
+}
+
+OP2
+
+foreach($array as $clave => $valor) {
+   $clave tendrá en cada iteracion una clave del array
+
+    $valor tendrá en cada iteracion el valor del array
+}
+*/
+$laptop=["Acer Nitro", "Windows11","AMD RYZEN", "16GB RAM", "1TB SSD", "NVIDIA RTX 3060"];
+
+
+
+foreach($laptop as $valor){
+    echo $valor."<br>";
+}
+?>
