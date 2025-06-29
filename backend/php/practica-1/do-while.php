804098b252a8512f75e2c2c760b6978985a8b481
hevieri <hevieri@duck.com>
2025-06-28 23:35:42 -0300

    prueba

---
+++ backend/php/practica-1/do-while.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+do{
+    // Código a ejecutar al menos una vez
+} while(condicion);
+*/
+$c=1;
+do {
+    echo $c . "<br>";
+    $c++;
+} while ($c <= 20);
+
+?>
---
+++ backend/php/practica-1/while.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+
+PRIMER OPCION:
+
+while(condicion) {
+    // Código a ejecutar mientras la condición sea verdadera
+}
+
+
+SEGUNDA OPCION:
+
+while(condicion):   
+    codigo a ejecutar mientras la condición sea verdadera
+endwhile;
+    
+*/
+
+    $c=1;
+
+    while($c <= 20) {
+        echo $c;
+        $c++;
+    }
+
+
+?>
