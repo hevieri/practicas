435f16b07427f2981b4807952f5fe833c587ec50
hevieri <107402659+hevieri@users.noreply.github.com>
2025-07-04 06:35:16 -0300

    Add files via upload

---
+++ backend/php/practica-2/GET/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Categorías</title>
+</head>
+<body>
+    <h1>Lista de Categorías</h1>
+    <ul>
+        <li><a href="productos.php?categoria=Ropa">Ropa</a></li>
+        <li><a href="productos.php?categoria=Calzado">Calzado</a></li>
+        <li><a href="productos.php?categoria=Electrónica">Electrónica</a></li>
+        
+    </ul>
+</body>
+</html>
